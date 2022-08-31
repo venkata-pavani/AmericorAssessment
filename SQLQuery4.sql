236c8e796242feb52345af5aac8941532bb7ec4c
Pavani N <12963112+venkata-pavani@users.noreply.github.com>
2022-08-30 19:27:04 -0700

    Add files via upload

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -3,7 +3,7 @@ left join [dbo].[department - department] as dt
 on tt.department_id = dt.id
 where dt.title = 'Sales')
 
-select count(distinct ISNULL(user_id,0)),st.team 
+select count(distinct ISNULL(user_id,0)) as head_count,st.team 
 from sales_teams as st left join [dbo].[user_team - user] as ut  on st.team_id = ut.team_id
 where cast(updated_at as date) <= '2019-12-14'
 group by st.team_id,team
@@ -11,7 +11,7 @@ group by st.team_id,team
 union 
 
 
-select 0,st.team from sales_teams as st 
+select 0 as head_count,st.team from sales_teams as st 
 where st.team_id not in (select team_id from [dbo].[user_team - user] where cast(updated_at as date) <= '2019-12-14')
 
 
